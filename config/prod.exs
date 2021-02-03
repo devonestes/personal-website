64654b58f93720260c21db048be901e701506069
Devon Estes <devon.c.estes@gmail.com>
2021-02-03 10:46:55 +0100

    force ssl in prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,11 @@
 use Mix.Config
 
 config :personal_website, PersonalWebsiteWeb.Endpoint,
-  url: [host: "www.devonestes.com"],
+  url: [host: "devonestes.com"],
   http: [port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
+  debug_errors: false,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :logger, level: :info
